5ffb992c474908d4aa4b0572d6cfc7de110fe5cd
Shubha1232 <95339673+Shubha1232@users.noreply.github.com>
2021-12-03 12:24:30 +0530

    add mail code

--- generate_steel_code_sub.php
+++ generate_steel_code_sub.php
@@ -11,16 +11,11 @@ $crud = new Database();
 
 $forger_tc_supplier = $_POST['generate_forger_tc_supplier'];
 
-$query = "SELECT email FROM email ";
-$email = $crud->getAllData($query);
-// echo "<pre>";
-// print_r($email);
-// die ;
+
 
 // $material_grade = $_POST['generate_material_grade'];
 
-  $heat_no = $_POST['generate_heat_no'];
-
+$heat_no = $_POST['generate_heat_no'];
 
 $query = "SELECT part_no,accept,mill_tc_supplier,forger_tc_supplier,heat_no,grade,part_grade FROM `metallurgical_report` WHERE heat_no = '$heat_no'";
 
@@ -30,10 +25,7 @@ $response = array();
 $req = "SELECT *  FROM steel_code WHERE heat_no='$heat_no'  AND forger_tc_supplier='$forger_tc_supplier'";
 
 $rst = $crud->getSingleRow($req);
-// echo "<pre>";
-// print_r($rst['steel_code']);
-// $heat_no;
-// die ;
+
 if($rst){
 
 echo json_encode(array('status' => 0, 'data' => 'Steel Code Already Generated'));
@@ -98,71 +90,11 @@ foreach ($rs as $key => $value) {
                   );
 
                   $result = $crud->insert('steel_code',$data);
-                  // code for send  mail 
-                  if($result){
-                     $query = "SELECT email FROM email ";
-                     $email = $crud->getAllData($query);
-                     // echo "<pre>";
-                     // print_r($email);
-                     // die ;
-                     foreach($email as $em){
-                       $Email =  $em['email'];
-                       
-                     
-                     $subject="ewayits.com - Generate Steel Code";
-                     //  $header = 'MIME-Version: 1.0'."\r\n";
-                     //  $header .= 'Content-type: text/html; charset=iso-8859-1'."\r\n";
-                     //  $header.="From:info@ewayits.com"; 
-                     //  $site_info = "ewayits.com/eicher";
-                     //  $message='<html><body>';
-                      $message.='Steel Code Generated';
-                      $message.="<br><br>";
-                      $message.='HEAT NO./STEEL CODE' .$heat_no .'/' .$rst['steel_code'] ;
-                      $message.="<br><br>";                    
-                      $message.='Team Eicher';
-                     $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-                     try {
-                     //Server settings
-                     $mail->SMTPDebug = 0;                                 // Enable verbose debug output
-                     $mail->isSMTP();                                      // Set mailer to use SMTP
-                     $mail->Host = 'mail.link4.in';  // Specify main and backup SMTP servers
-                     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-                     $mail->Username = 'info@link4.in';                 // SMTP username
-                     $mail->Password = 'Ujjain@0734';                           // SMTP password
-                     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-                     $mail->Port = 587;                                    // TCP port to connect to
-                  $mail->SMTPOptions = array(
-                     'ssl' => array(
-                         'verify_peer' => false,
-                         'verify_peer_name' => false,
-                         'allow_self_signed' => true
-                     )
-                 );
-                 
-                     //Recipients
-                     $mail->setFrom('info@link4.in', '');
-                     $mail->addAddress($Email, '');     // Add a recipient
-                     //Content
-                     $mail->isHTML(true);                                  // Set email format to HTML
-                     $mail->Subject = $subject;
-                     $mail->Body    = $message;
-                     //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-                 
-                     $mail->send();
-                     //echo 'Message has been sent';
-                     
-                 } catch (Exception $e) {
-                    
-                    // echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-                     
-                 }
-                  }
-
                   
-                   }
-                   $response[$key1]['part_no'] = $value1;
+                  $response[$key1]['part_no'] = $value1;
 
                   $response[$key1]['steel_code'] = $code;
+
                }
 
          
@@ -173,10 +105,12 @@ foreach ($rs as $key => $value) {
 
 
 
+
 if($response){
 
-   // print_r($rs); die;
-
+   // print_r($response); 
+   
+  
 	$up_data = array('is_report_generated' => 1);
 
 	$where = "heat_no = '$heat_no'";
@@ -184,6 +118,94 @@ if($response){
 	$crud->update("metallurgical_report", $up_data, $where); 
 
 	echo json_encode(array('status' => 1, 'data' => $response));
+   
+  
+
+  // get logged in user email 
+  $id = $_SESSION["user_id"];
+  $query = "SELECT email FROM users where id =  '$id'  ";
+  $loggedInEMail = $crud->getSingleRow($query);
+
+   
+   // getting all users  mail 
+   $query = "SELECT email FROM email ";
+   $result = $crud->getAllData($query);
+   
+   // sending mail
+   foreach($result as $emails )
+   {
+      $Email = $emails['email'];
+   
+  
+
+     
+  
+   $subject="ewayits.com - Steel Code Generate";
+   //  $header = 'MIME-Version: 1.0'."\r\n";
+   //  $header .= 'Content-type: text/html; charset=iso-8859-1'."\r\n";
+   //  $header.="From:info@ewayits.com"; 
+   //  $site_info = "ewayits.com/eicher";
+   //  $message='<html><body>';
+  
+
+  $message = ""; 
+  $message.= '<table border="1" style="border-collapse: collapse;"><tr><th>S.NO.</th><th>PART NO.</th><th>STEEL CODE</th></tr>';
+  $i = 1;
+   foreach($response as $val){
+
+    
+   $message.=   "<tr><td>".$i.".</td><td>".$val['part_no']."</td><td>".$val['steel_code']."</td></tr>";
+  
+   $i++;
+     }
+    
+   $message.= '</table>';
+   $message.="<br>";
+   $message.="The following steel code generated by " .$_SESSION['user_name']."(".$loggedInEMail['email'].")";
+   $message.="<br>";
+
+   $message.= "Date-". date("d/M/Y");
+   $message.="<br>";
+   $message.='Team Eicher';
+   $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+   try {
+   //Server settings
+   $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+   $mail->isSMTP();                                      // Set mailer to use SMTP
+   $mail->Host = 'smtp.mailtrap.io';  // Specify main and backup SMTP servers
+   $mail->SMTPAuth = true;                               // Enable SMTP authentication
+   $mail->Username = 'e423a89fb6dfcd';                 // SMTP username
+   $mail->Password = 'd20e39e7d3e60a';                           // SMTP password
+   $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+   $mail->Port = 2525;                                    // TCP port to connect to
+   $mail->SMTPOptions = array(
+   'ssl' => array(
+       'verify_peer' => false,
+       'verify_peer_name' => false,
+       'allow_self_signed' => true
+   )
+);
+  
+   //Recipients
+   $mail->setFrom('shubham.eway@gmail.com', '');
+   $mail->addAddress($Email, '');     // Add a recipient
+   //Content
+   $mail->isHTML(true);                                  // Set email format to HTML
+   $mail->Subject = $subject;
+   $mail->Body    = $message;
+   //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+   $mail->send();
+   //echo 'Message has been sent';
+   
+} catch (Exception $e) {
+  
+  // echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+   
+}
+
+
+}
 
 }
 
@@ -194,6 +216,14 @@ echo json_encode(array('status' => 0,'data' => 'No Availaible Part No. For This 
 
 
   }
+
+
+   
+
+
+
+
+
 
 }
 
